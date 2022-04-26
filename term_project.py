1c693ad93b86b0f03639cd78ee64e9977efdb3e7
DESKTOP-4NDCLE1\OH.S.D <erfrr@naver.com>
2022-04-27 01:24:22 +0900

    텀 프로젝트 파일 매니저 GUI 구성 3
    검색할 파일 확장자 선택하는 라디오 버튼 생성 (txt, jpg)

--- term_project.py
+++ term_project.py
@@ -1,12 +1,34 @@
-import tkinter
+import logging
+from tkinter import *
+from tkinter.ttk import *
+from tkinter.scrolledtext import ScrolledText
 
 
-def exit(event=None):
+def quit_window(event=None):
     windows.quit()
 
 
-windows = tkinter.Tk()
+def found_extension_value():
+    logging.debug(found_extension.get())
+
+
+windows = Tk()
 windows.title("File_Manager")
 
-windows.bind("<Escape>", exit)
+# ---------------- 파일 검색 용 ------------
+# 파일 확장자 설정
+# 파일 확장자 선택을 위한 라디오 버튼
+
+file_extension_frame = LabelFrame(text="Extension")
+file_extension_frame.pack()
+
+found_extension = StringVar()
+txt_radioButton = Radiobutton(file_extension_frame, command=found_extension_value, text="txt", value=".txt",
+                              variable=found_extension)
+jpg_radioButton = Radiobutton(file_extension_frame, command=found_extension_value, text="jpg", value=".jpg",
+                              variable=found_extension)
+txt_radioButton.pack()
+jpg_radioButton.pack()
+
+windows.bind("<Escape>", quit_window)
 windows.mainloop()
