8eff6ce9567581ff0f8de19f3e6d4bb467b3ec32
DESKTOP-4NDCLE1\OH.S.D <erfrr@naver.com>
2022-04-27 13:47:27 +0900

    텀 프로젝트 파일 매니저 GUI 구성 3
    -검색할  파일 경로를 입력하는 텍스스 창 + 탐색 버튼 구현
    -탐색 수행시 리스트 박스에 해당 경로에 존재하는 파일들을 출력하게 했음. <버그 존재>

--- term_project.py
+++ term_project.py
@@ -1,4 +1,4 @@
-import logging
+import os
 from tkinter import *
 from tkinter.ttk import *
 from tkinter.scrolledtext import ScrolledText
@@ -8,8 +8,18 @@ def quit_window(event=None):
     windows.quit()
 
 
-def found_extension_value():
-    logging.debug(found_extension.get())
+def set_path():
+    search_file(input_path.get(), 0)
+
+
+def search_file(root_path, lst_idx):
+    found_result_lstBox.delete(0, END)
+    files = os.listdir(root_path)
+    for file in files:
+        path = os.path.join(root_path, file)
+        found_result_lstBox.insert(lst_idx, path)
+        if os.path.isdir(path):
+            search_file(path, lst_idx + 1)
 
 
 windows = Tk()
@@ -18,17 +28,34 @@ windows.title("File_Manager")
 # ---------------- 파일 검색 용 ------------
 # 파일 확장자 설정
 # 파일 확장자 선택을 위한 라디오 버튼
-
 file_extension_frame = LabelFrame(text="Extension")
-file_extension_frame.pack()
+file_extension_frame.pack(side=RIGHT)
 
 found_extension = StringVar()
-txt_radioButton = Radiobutton(file_extension_frame, command=found_extension_value, text="txt", value=".txt",
+txt_radioButton = Radiobutton(file_extension_frame, text="txt", value=".txt",
                               variable=found_extension)
-jpg_radioButton = Radiobutton(file_extension_frame, command=found_extension_value, text="jpg", value=".jpg",
+jpg_radioButton = Radiobutton(file_extension_frame, text="jpg", value=".jpg",
+                              variable=found_extension)
+pdf_radioButton = Radiobutton(file_extension_frame, text="pdf", value=".jpg",
                               variable=found_extension)
 txt_radioButton.pack()
 jpg_radioButton.pack()
+pdf_radioButton.pack()
+# -------------------------------------------
 
+# 탐색할 파일 설정
+
+file_find_frame = Frame()
+file_find_frame.pack()
+input_path = Entry(file_find_frame, width=30)
+input_path.pack(side=LEFT)
+
+find_button = Button(file_find_frame, text="찾기", command=set_path)
+find_button.pack(side=RIGHT)
+
+# 탐색 결과 리스트
+
+found_result_lstBox = Listbox(selectmode='single', width=50)
+found_result_lstBox.pack(side=BOTTOM)
 windows.bind("<Escape>", quit_window)
 windows.mainloop()
