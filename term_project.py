6acdbb7c821af2c9771266d449c0063120b810a9
DESKTOP-4NDCLE1\OH.S.D <erfrr@naver.com>
2022-04-27 14:47:41 +0900

    텀 프로젝트 파일 매니저 GUI 구성 5
    - 스크롤 텍스트 -> 리스트 박스로 변경 , 배경색깔 설정
    - 이상한 파일경로 /  라디오 버튼 미선택 예외처리

--- term_project.py
+++ term_project.py
@@ -2,6 +2,7 @@ import os
 from tkinter import *
 from tkinter.ttk import *
 from tkinter.scrolledtext import ScrolledText
+import tkinter.messagebox as messagebox
 
 
 def quit_window(event=None):
@@ -13,13 +14,27 @@ def set_path():
 
 
 def search_file(root_path, lst_idx):
-    found_result_lstBox.delete(0, END)
-    files = os.listdir(root_path)
-    for file in files:
-        path = os.path.join(root_path, file)
-        found_result_lstBox.insert(lst_idx, path)
-        if os.path.isdir(path):
-            search_file(path, lst_idx + 1)
+    if lst_idx == 0:
+        found_result_lstBox.delete(0, END)
+
+    # 확장자 선택 안했을때 예외처리
+    if found_extension.get() == '':
+        messagebox.showerror("에러", "탐색 확장자를 설정해 주시오.")
+        return
+
+    if os.path.exists(root_path):
+        files = os.listdir(root_path)
+        for file in files:
+            path = os.path.join(root_path, file)
+            print(path)
+            found_result_lstBox.insert(lst_idx, path)
+            if os.path.isdir(path):
+                search_file(path, lst_idx + 1)
+            else:
+                lst_idx += 1
+    else:
+        messagebox.showinfo("에러", "올바른 경로를 설정하시오.")
+        return
 
 
 windows = Tk()
@@ -36,26 +51,29 @@ txt_radioButton = Radiobutton(file_extension_frame, text="txt", value=".txt",
                               variable=found_extension)
 jpg_radioButton = Radiobutton(file_extension_frame, text="jpg", value=".jpg",
                               variable=found_extension)
-pdf_radioButton = Radiobutton(file_extension_frame, text="pdf", value=".jpg",
-                              variable=found_extension)
+pptx_radioButton = Radiobutton(file_extension_frame, text="pptx", value=".pptx",
+                               variable=found_extension)
 txt_radioButton.pack()
 jpg_radioButton.pack()
-pdf_radioButton.pack()
+pptx_radioButton.pack()
 # -------------------------------------------
 
-# 탐색할 파일 설정
+# 탐색할 파일 입력 창
 
 file_find_frame = Frame()
-file_find_frame.pack()
-input_path = Entry(file_find_frame, width=30)
+file_find_frame.pack(side=LEFT)
+input_path = Entry(file_find_frame, width=50)
 input_path.pack(side=LEFT)
-
 find_button = Button(file_find_frame, text="찾기", command=set_path)
 find_button.pack(side=RIGHT)
 
 # 탐색 결과 리스트
-
-found_result_lstBox = Listbox(selectmode='single', width=50)
+found_result_lstBox = Listbox(selectmode='single', width=100)
 found_result_lstBox.pack(side=BOTTOM)
+found_result_lstBox.configure(background="skyblue", foreground="white", font='Aerial')
+
+# 파일 삭제 기능
+
+# 프로그램 종료 키워드
 windows.bind("<Escape>", quit_window)
 windows.mainloop()
